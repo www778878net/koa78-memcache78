656025a6fe8e2f2a222f02dc21f763e7dd13ed06
Frieda.Hu <www778878net@189.cn>
2024-10-22 11:50:59 +0800

    del console

--- src/index.ts
+++ src/index.ts
@@ -94,10 +94,10 @@ class MemCache78 {
         key += this.local;
         try {
             const result: MemcachedResponse = await this.client.add(key, value, { expires: sec });
-            console.log('Add result:', result);
+            //console.log('Add result:', result);
             return result;
         } catch (error) {
-            console.error('Error in add method:', error);
+            //console.error('Error in add method:', error);
             if (error instanceof Error && error.message.includes('NOT_STORED')) {
                 // 键已经存在
                 return false;
